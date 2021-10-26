3d69b5340a670eaae3dfac2ab0782b66f5bd9357
Carlos Ferreira <conta@especializati.com.br>
2021-10-26 18:31:59 -0300

    Dockerfile APP Golang

---
+++ dockerfiles/golang/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+    "fmt"
+    "html"
+    "net/http"
+)
+
+func main() {
+
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+    })
+
+    http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request){
+        fmt.Fprintf(w, "Hi")
+    })
+
+    http.ListenAndServe(":8081", nil)
+    
+}
